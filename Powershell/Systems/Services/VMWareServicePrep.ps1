20b10d315a0c3ce1154c6ee112dc1553ea8465e1
Kevin McClean <6920876+GogoGit@users.noreply.github.com>
2021-02-02 17:55:47 -0500

    Update VMWareServicePrep.ps1
    Tested on SQL Server
    Need to test on IIS Server
    
    -- NO Error Coding
    -- Added the ability to email Results
    -- Updated the way it runs on a Server, it will check to see if the following Components are installed and will Start/Stop them accordingly
    -----> Cluster Server
    -----> EMS Service(s)
    -----> IIS
    -----> SQL Service(s)

--- Powershell/Systems/Services/VMWareServicePrep.ps1
+++ Powershell/Systems/Services/VMWareServicePrep.ps1
@@ -1,4 +1,20 @@
 <#
+*** You will need to change the execution policy for Powershell if running this via the Powershell ISE
+    *Must Have Admin Access to Do this
+    
+    #Will show current Policy
+    Get-ExecutionPolicy   
+
+    #Will Allow you to Run Script
+    Set-ExecutionPolicy -ExecutionPolicy Unrestricted   
+
+    #Will Set the Execution Policy Back to the Default Settings
+    Set-ExecutionPolicy -ExecutionPolicy Default        
+
+
+Resources:
+    About special Characters:  https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_special_characters?view=powershell-7.1
+
 Starting and Stopping Windows Services
 --------------------------------------
 
@@ -39,28 +55,46 @@ How to Change Startup State
 
 #>
 
+#Default Variables
+#----------------
+#Text Editing
+#------------
 New-Variable -Scope global -name CRLF -Value "`r`n"
-    
+New-Variable -Scope global -Name TAB -Value "`t"
+
 #Error Config
 #------------
 #Preference
     #$ErrorActionPreference = "STOP"
     $ErrorActionPreference = "Continue"
     #$ErrorActionPreference = "SilentlyContinue"
+
 New-Variable -Scope global -Name gbolChange -Value $False
 New-Variable -Scope global -Name gbolError -Value $False
 New-Variable -Scope global -Name LogMessage -Value ""
 
 New-Variable -Scope global -Name gbolDebug -Value $True
 
+#Email Settings
+New-Variable -Scope global -Name gstrEmailFrom -Value ($env:COMPUTERNAME + ".NoReply@NYU.EDU").ToString()
+New-Variable -Scope global -Name gstrEmailSubject -Value ((Get-Date -Format "yyyyMMdd-hh:mm").ToString() + " VMWare Service Prep")
+
+
 ##Dev or Prod
 New-Variable -Scope global -Name gbolProd -Value $False
 If($gbolProd -eq $False){
     #Dev
+    #Email Settings
+    New-Variable -Scope global -Name gstrEmailTo -Value "km193@nyu.edu"
 }
 Else{
     #Prod
+    #-----
+    #Email Settings
+    New-Variable -Scope global -Name gstrEmailTo -Value "l5b4z8w8i9w1x4h5@nyumeyers.slack.com"
 }
+
+
 
 #Event Log
 $strLogName = "Application"
@@ -73,10 +107,28 @@ enum enumServiceState{
     Stop = 2
 }
 
-#enum enumServiceGroup{
-#    IIS = 1
-#    SQL = 2
-#}
+
+Function UpdateString(){
+    Param(
+        [Parameter(Mandatory=$true)]
+        [String]$strInfo = "",
+        [Parameter(Mandatory=$true)]
+        [String]$strUpdate = "",
+        [Parameter(Mandatory=$false)]
+        [Boolean]$bolNewLine = $true
+    )
+
+    <#
+        $strInfo is not set to Mandatory since an error will occur if that variable is blank/Null
+    #>
+
+    If ($strInfo.Trim().Length -gt 0){
+        If($bolNewLine -eq $true){Return ($strInfo.Trim() + $CRLF +$strUpdate)}
+        Else{Return ($strInfo.Trim() + $strUpdate)}
+    }
+    Else{Return $strUpdate}
+}
+
 
 Function UpdateLogMessage(){
     Param(
@@ -103,46 +155,42 @@ Function UpdateLogMessage(){
     }
 }
 
-Function ModifyService(){
+
+Function SendEmail(){
     Param(
         [Parameter(Mandatory=$true)]
-        [enumServiceState]$ServiceState,
+        [String]$strEmailTo = $gstrEmailTo,
         [Parameter(Mandatory=$true)]
-        [String]$strServiceName
+        [String]$strEmailFrom= $gstrEmailFrom,
+        [Parameter(Mandatory=$False)]
+        [String]$strEmailSubject = $gstrEmailSubject,
+        [Parameter(Mandatory=$true)]
+        [String]$strEmailBody,
+        [Parameter(Mandatory=$false)]
+        [String]$strEmailMsg="",
+        [Parameter(Mandatory=$false)]
+        [String]$strEmailAttachementPath=""
+
     )
-
-    #Debug: Default Values
-    If($Global:gbolDebug -eq $true){
-        Write-Host "Modify Service Default Values"
-        Write-Host $ServiceState
-        Write-Host $strServiceName
-        Write-Host "---------------------------"
-
-    }
-
-
-    If($ServiceState -eq [enumServiceState]::Start){
-        #Start
-        If ((Get-Service -Name $strServiceName).Status -ne "Running"){
-            #Debug
-            If($Global:gbolDebug -eq $true){Write-Host "Start Service '" + $strServiceName+ "'"}
-            Start-Service -Name $strServiceName
-
+    
+    #SMTP server name
+    $SMTPServer = "smtp.nyu.edu"
+    #Create a Mail object
+    $SMTPMessage = New-Object Net.Mail.MailMessage($strEmailFrom, $strEmailTo, $strEmailSubject, $strEmailBody)
+    #Create an Attachment Objct
+    If($strEmailAttachementPath.Length -gt 0){
+        If((Test-Path -Path $strEmailAttachementPath.Trim()) -eq $true){
+            $SMTPMessage.Attachments.Add($Attachment)
+            $Attachment = New-Object Net.Mail.Attachment($strEmailAttachementPath)
         }
-        #Debug
-        If($Global:gbolDebug -eq $true){Write-Host "Change Startup to Automatic for '" + $strServiceName + "'"}
-        Set-service -Name $strServiceName -StartupType Automatic
+        Else{$strEmailBody = $CRLF + "WARNING - Check Attachment Path: " + $strEmailAttachementPath + $CRLF}
     }
-    Else{
-        #Stop
-        #Debug
-        If($Global:gbolDebug -eq $true){Write-Host "Stop Service '" + $strServiceName+ "'"}
-        Stop-Service -Force -Name $strServiceName
 
-        #Debug
-        If($Global:gbolDebug -eq $true){Write-Host "Change Startup to Manual for '" + $strServiceName + "'"}
-        set-service -Name $strServiceName -StartupType Manual
-    }
+    #Create SMTP server object
+    $SMTPClient = New-Object Net.Mail.SmtpClient($SmtpServer) 
+    #Send
+    $SMTPClient.Send($SMTPMessage)    
+
 }
 
 
@@ -152,26 +200,20 @@ Function VMWareMove(){
         [enumServiceState]$ServiceState
     )
 
+    <#
+        IIS Service needs to be handled differently as we are not stopping the IISAdmin Service.
+        We are using a different method to start and stop the Web Sites
+        --->  iisreset /Start
+        --->  iisreset /Stop
+    #>
 
 
-    #EMS
-    $objServices = Get-Service -DisplayName "EMS*"
+    $arryServiceSearch = @('Cluster*','EMS*', 'SQL*')
+    [Object]$arryTmp
+    [Object]$arryServices
+    [String]$strSettingChanges = $env:COMPUTERNAME + $CRLF + "---------------------------------------" + $CRLF 
 
-    If ($objServices.Count -gt 0){
-        #Debug
-        If($Global:gbolDebug -eq $true){Write-Host "EMS Section" + $CRLF + "---------------------------------------"}
-
-        #Process Services
-        For ($i=0; $i -le ($objServices.Count -1);$i++){
-            #Debug
-            If($Global:gbolDebug -eq $true){Write-Host "Processing Count : " ($i+1) + ", Service Name: " + $objServices[$i].Name}
-        
-            #Services
-            If($ServiceState -eq [enumServiceState]::Start){ModifyService -ServiceState Start -strServiceName $objServices[$i].Name.ToString()}
-            Else{ModifyService -ServiceState Stop -strServiceName $objServices[$i].Name.ToString()}
-        }
-    }
-
+    #Determine if IIS is installed on the Server
     #IIS
     $objServices = Get-Service -DisplayName "IIS*"
 
@@ -179,35 +221,73 @@ Function VMWareMove(){
         #Debug
         If($Global:gbolDebug -eq $true){Write-Host "IIS Section" + $CRLF + "---------------------------------------"}
 
-        #Process Services
-        For ($i=0; $i -le ($objServices.Count -1);$i++){
-            #Debug
-            If($Global:gbolDebug -eq $true){Write-Host "Processing Count : " ($i+1) + ", Service Name: " + $objServices[$i].Name}
+        $strSettingChanges = UpdateString -strInfo $strSEttingChanges -strUpdate ("IIS Section" + $CRLF + "----------" + $CRLF )
+
+        #Start/Stop Website(s)
+        If($ServiceState -eq [enumServiceState]::Start){
+            iisreset /start
+            $strSettingChanges = UpdateString -strInfo $strSEttingChanges -strUpdate ($TAB + "IIS Started" + $CRLF + "----------" + $CRLF )
+        }
+        Else{
+            iisreset /stop
+            $strSettingChanges = UpdateString -strInfo $strSEttingChanges -strUpdate ($TAB + "IIS Stopped" + $CRLF + "--------------------" + $CRLF )
+        }
+
+    }
+    
+    #Determine What other Services Are Installed
+    $arryServiceSearch | ForEach-Object {
+        $objServices = Get-Service -DisplayName $_
         
-            #Services
-            If($ServiceState -eq [enumServiceState]::Start){iisreset /start}
-            Else{iisreset /stop}
+        If ($objServices.Count -gt 0){
+            $strSettingChanges = UpdateString -strInfo $strSettingChanges -strUpdate ($_ + " Section" + $CRLF + "----------" + $CRLF )
+            
+            #Modify Services
+            For ($i=0; $i -le ($objServices.Count -1);$i++){
+                #Debug
+                If($Global:gbolDebug -eq $true){Write-Host $objServices[$i].Name + " Section" + $CRLF + "---------------------------------------"}
+                $strSettingChanges = UpdateString -strInfo $strSettingChanges -strUpdate ($TAB + $objServices[$I].Name +  $CRLF)
+                
+                If($ServiceState -eq [enumServiceState]::Start){
+                    #Start Service
+                    If($objServices[$i].Status -ne "Running" ){
+                        $strSettingChanges = UpdateString -strInfo $strSettingChanges -strUpdate ($TAB + $TAB + "Status: " + $objServices[$I].Status +  " ---> Started" + $CRLF )
+                        Start-Service -Name ($objServices[$i].Name)
+                    }
+                    Else{$strSettingChanges = UpdateString -strInfo $strSettingChanges -strUpdate ($TAB + $TAB + "Status: " + $objServices[$I].Status +  " ---> NO CHANGE" + $CRLF )}
+                    
+                    #Change Startup Option
+                    If($objServices[$i].StartType -ne "Automatic"){
+                        $strSettingChanges = UpdateString -strInfo $strSettingChanges -strUpdate ($TAB + $TAB + "StartUp: " + $objServices[$I].StartType +  " ---> Automatic" + $CRLF )
+                        Set-service -Name ($objServices[$i].Name) -StartupType Automatic
+                    }
+                    Else{$strSettingChanges = UpdateString -strInfo $strSettingChanges -strUpdate ($TAB + $TAB + "StartUp: " + $objServices[$I].StartType +  " ---> NO CHANGE" + $CRLF )}
+                }
+                Else{
+                    #Stop Service
+                    If($objServices[$i].Status -ne "Stopped" ){
+                        $strSettingChanges = UpdateString -strInfo $strSettingChanges -strUpdate ($TAB + $TAB + "Status: " + $objServices[$I].Status +  " ---> Stopped" + $CRLF )
+                        Stop-Service -Force -Name ($objServices[$i].Name)
+                    }
+                    Else{$strSettingChanges = UpdateString -strInfo $strSettingChanges -strUpdate ($TAB + $TAB + "Status: " + $objServices[$I].Status +  " ---> NO CHANGE" + $CRLF )}
+                    
+                    #Change Startup Option
+                    If($objServices[$i].StartType -ne "Manual"){
+                        $strSettingChanges = UpdateString -strInfo $strSettingChanges -strUpdate ($TAB + $TAB + "StartUp: " + $objServices[$I].StartType +  " ---> Manual" + $CRLF )
+                        Set-service -Name ($objServices[$i].Name) -StartupType Manual
+                    }
+                    Else{$strSettingChanges = UpdateString -strInfo $strSettingChanges -strUpdate ($TAB + $TAB + "StartUp: " + $objServices[$I].StartType +  " ---> NO CHANGE" + $CRLF )}
+                }
+            }
         }
     }
 
-    #SQL Server
-    $objServices = Get-Service -DisplayName "SQL*"
-
-    If ($objServices.Count -gt 0){
-        #Debug
-        If($Global:gbolDebug -eq $true){Write-Host "SQL Server Section" + $CRLF + "---------------------------------------"}
 
 
-        #Process Services
-        For ($i=0; $i -le ($objServices.Count -1);$i++){
-            #Debug
-            If($Global:gbolDebug -eq $true){Write-Host "Processing Count : " ($i+1) + ", Service Name: " + $objServices[$i].Name}
-        
-            #Services
-            If($ServiceState -eq [enumServiceState]::Start){ModifyService -ServiceState Start -strServiceName $objServices[$i].Name.ToString()}
-            Else{ModifyService -ServiceState Stop -strServiceName $objServices[$i].Name.ToString()}
-        }
-    }
+    Write-Host $strSettingChanges
+
+    #Sent Email
+    SendEMail -strEmailTo $gstrEmailTo -strEmailFrom $gstrEmailFrom -strEmailSubject $gstrEmailSubject -strEmailBody $strSettingChanges
 
     Write-Host ""
     Write-Host ""
@@ -219,7 +299,6 @@ Function VMWareMove(){
     Write-Host "TASK COMPLETED!"
     Write-Host "------------------------------"
 }
-
 
 
 Write-Host "To Change the EMS Services run one of the following commands.."
